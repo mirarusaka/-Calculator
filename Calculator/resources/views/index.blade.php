86338d96b636a54a46a96ce81942b6f6fd948d46
69safe <endokun32@yahoo.co.jp>
2021-08-15 19:25:15 +0900

    viewテンプレート作成、背景設定、sass自動コンパイル機能追加

--- Calculator/resources/views/index.blade.php
+++ Calculator/resources/views/index.blade.php
@@ -1,17 +1,7 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-</head>
-<body>
-  <div>
-    </head>
-    <body class="antialiased">
-        <div class="d-flex justify-content-center">
-            <h1>計算機プログラムです！</h1>
-        </div>
-    </body>
-</html>
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('contents')
+        <h1>計算機プログラムです！</h1>
+@endsection
---
+++ Calculator/resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>Oyama Calculator - @yield('title')</title>
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+  <div>
+    </head>
+    <body>
+        <h2>大山の計算機プログラム</h2>
+        <div class="container">
+            @yield('contents')
+        </div>
+    </body>
+</html>
