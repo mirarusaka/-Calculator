cc6a29af35a9c6281471e85a7ac5349e190b5848
69safe <endokun32@yahoo.co.jp>
2021-08-25 23:19:26 +0900

    計算機のレイアウトベースを作成

--- Calculator/resources/views/main.blade.php
+++ Calculator/resources/views/main.blade.php
@@ -3,11 +3,17 @@
 @section('title', 'main')
 
 @section('contents')
-        C +/- % ÷<br>
-        1 2 3 ×<br>
-        4 5 6 -<br>
-        7 8 9 +<br>
-        0   . =
+
+<div class="box">
+    ここに数値が入ります<br>
+    {{-- ボタン --}}
+    C +/- % ÷<br>
+    1 2 3 ×<br>
+    4 5 6 -<br>
+    7 8 9 +<br>
+    0   . =
+</div>
+
 
 
 @endsection
