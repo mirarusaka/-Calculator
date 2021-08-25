a22d2920ee4acac66480964586489b73f19d3b67
69safe <endokun32@yahoo.co.jp>
2021-08-25 23:05:59 +0900

    計算機メイン画面

---
+++ Calculator/resources/views/main.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('title', 'main')
+
+@section('contents')
+        C +/- % ÷<br>
+        1 2 3 ×<br>
+        4 5 6 -<br>
+        7 8 9 +<br>
+        0   . =
+
+
+@endsection
