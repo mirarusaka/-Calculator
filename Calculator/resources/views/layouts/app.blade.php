29531e59e1ecf5771d931413f79aaa0cc5105e2a
69safe <endokun32@yahoo.co.jp>
2021-08-26 22:30:40 +0900

    bootstrap導入

--- Calculator/resources/views/layouts/app.blade.php
+++ Calculator/resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <title>Oyama Calculator - @yield('title')</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
   <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
@@ -14,5 +15,6 @@
             <div class="right">
                 ©Norihisa Oyama 2021.
             </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
     </body>
 </html>
