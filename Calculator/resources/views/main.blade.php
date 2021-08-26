63a1b4f287c385fda2c3bc87f60d9379ad7ff340
69safe <endokun32@yahoo.co.jp>
2021-08-26 23:30:09 +0900

    計算機レイアウト整理

--- Calculator/resources/views/main.blade.php
+++ Calculator/resources/views/main.blade.php
@@ -6,15 +6,38 @@
 
 <div class="box">
     <ul>
-        ここに数値が入ります<br>
+        １２３４５６７８９　<br>
     </ul>
-    {{-- ボタン それぞれリンクがつく --}}
+    {{-- ボタン それぞれリンクがつく Bootstrap5 使用--}}
     <p>
-        C +/- % ÷<br>
-        1 2 3 ×<br>
-        4 5 6 -<br>
-        7 8 9 +<br>
-        0   . =
+        {{-- １行目 --}}
+        <button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">C</button>
+        <button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">±</button>
+        <button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">%</button>
+        <button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">÷</button>
+        <br>
+        {{-- 2行目 --}}
+        <button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">1</button>
+        <button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">2</button>
+        <button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">3</button>
+        <button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">×</button>
+        <br>
+        {{-- 3行目 --}}
+        <button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">4</button>
+        <button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">5</button>
+        <button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">6</button>
+        <button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">-</button>
+        <br>
+        {{-- 4行目 --}}
+        <button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">7</button>
+        <button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">8</button>
+        <button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">9</button>
+        <button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">+</button>
+        <br>
+        {{-- 5行目 0のみ長丸ボタン --}}
+        <button type="button" class="btn btn-primary rounded-pill" style="width:8rem;height:4rem;">0　　　</button>
+        <button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">.</button>
+        <button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">=</button>
     </p>
 </div>
 
