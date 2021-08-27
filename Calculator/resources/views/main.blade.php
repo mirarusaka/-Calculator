f875eb968e3175f6b92a30ab104bc9aba05ccc17
69safe <endokun32@yahoo.co.jp>
2021-08-27 09:51:14 +0900

    bootstrapのボタンカラーを改造。

--- Calculator/resources/views/main.blade.php
+++ Calculator/resources/views/main.blade.php
@@ -5,16 +5,15 @@
 @section('contents')
 
 <div class="box">
-    <ul> {{-- ここに数字を表示。 --}}
+    <p> {{-- ここに数字を表示。 --}}
         @if (isset($number))
             {{ $number }}
         @else
-            0
+            -1,345,678,900,000
         @endif
         <br>
-    </ul>
+    </p>
     {{-- ボタン それぞれリンクがつく Bootstrap5 使用--}}
-    <p>
         {{-- １行目 --}}
         <button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">C</button>
         <button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">±</button>
@@ -40,10 +39,9 @@
         <button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">+</button>
         <br>
         {{-- 5行目 0のみ長丸ボタン --}}
-        <button type="button" class="btn btn-primary rounded-pill" style="width:8rem;height:4rem;">0　　　</button>
+        <button type="button" class="btn btn-primary rounded-pill" style="width:8rem;height:4rem;">0</button>
         <button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">.</button>
         <button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">=</button>
-    </p>
 </div>
 
 
