f4db9efe202ee3b32d83b04e76b3874424ce327d
69safe <endokun32@yahoo.co.jp>
2021-08-28 08:21:14 +0900

    補足記載

--- Calculator/app/Http/Controllers/MainController.php
+++ Calculator/app/Http/Controllers/MainController.php
@@ -3,15 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\MathController;
+use App\Http\Controllers\NumberController;
 
-class MainController extends Controller
+class MainController extends Controller //ページの移動など、メイン動作のみ
 {
-    public function index()
+    public function index() //トップページ
     {
         return view('index');
     }
 
-    public function main()
+    public function main() //計算機メイン画面
     {
         $number = null;
 
--- Calculator/app/Http/Controllers/MathController.php
+++ Calculator/app/Http/Controllers/MathController.php
@@ -4,7 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class MathController extends Controller
+class MathController extends Controller //計算用
 {
-    //
+    public function all_clear($number)
+    {
+        $number = null;
+
+        return view('main', ['number' => $number]);
+    }
 }
