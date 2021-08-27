da95a355cc63ea332359456a32b5d0c0ab21a577
69safe <endokun32@yahoo.co.jp>
2021-08-28 08:21:43 +0900

    ボタンをaタグからformタグに変更。　<div style="display:inline-flex">　で横並びを実現

--- Calculator/resources/views/main.blade.php
+++ Calculator/resources/views/main.blade.php
@@ -15,33 +15,43 @@
     </p>
     {{-- ボタン それぞれリンクがつく Bootstrap5 使用--}}
         {{-- １行目 --}}
-        <a href=""><button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">AC</button></a>
-        <a href=""><button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">C</button></a>
-        <a href=""><button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">%</button></a>
-        <a href=""><button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">÷</button></a>
+        <div style="display:inline-flex">
+            <form metod="post" action=""><button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">AC</button></form>
+            <form metod="post" action=""><button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">C</button></form>
+            <form metod="post" action=""><button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">%</button></form>
+            <form metod="post" action=""><button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">÷</button></form>
+        </div>
         <br>
         {{-- 2行目 --}}
-        <a href=""><button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">1</button></a>
-        <a href=""><button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">2</button></a>
-        <a href=""><button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">3</button></a>
-        <a href=""><button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">×</button></a>
+        <div style="display:inline-flex">
+            <form metod="post" action=""><button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">1</button></form>
+            <form metod="post" action=""><button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">2</button></form>
+            <form metod="post" action=""><button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">3</button></form>
+            <form metod="post" action=""><button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">×</button></form>
+        </div>
         <br>
         {{-- 3行目 --}}
-        <a href=""><button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">4</button></a>
-        <a href=""><button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">5</button></a>
-        <a href=""><button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">6</button></a>
-        <a href=""><button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">-</button></a>
+        <div style="display:inline-flex">
+            <form metod="post" action=""><button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">4</button></form>
+            <form metod="post" action=""><button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">5</button></form>
+            <form metod="post" action=""><button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">6</button></form>
+            <form metod="post" action=""><button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">-</button></form>
+        </div>
         <br>
         {{-- 4行目 --}}
-        <a href=""><button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">7</button></a>
-        <a href=""><button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">8</button></a>
-        <a href=""><button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">9</button></a>
-        <a href=""><button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">+</button></a>
+        <div style="display:inline-flex">
+            <form metod="post" action=""><button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">7</button></form>
+            <form metod="post" action=""><button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">8</button></form>
+            <form metod="post" action=""><button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">9</button></form>
+            <form metod="post" action=""><button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">+</button></form>
+        </div>
         <br>
         {{-- 5行目 0のみ長丸ボタン --}}
-        <a href=""><button type="button" class="btn btn-primary rounded-pill" style="width:8rem;height:4rem;">0</button></a>
-        <a href=""><button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">.</button></a>
-        <a href=""><button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">=</button></a>
+        <div style="display:inline-flex">
+            <form metod="post" action=""><button type="button" class="btn btn-primary rounded-pill" style="width:8rem;height:4rem;">0</button></form>
+            <form metod="post" action=""><button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">.</button></form>
+            <form metod="post" action=""><button type="button" class="btn btn-primary rounded-circle p-0" style="width:4rem;height:4rem;">=</button></form>
+        </div>
 </div>
 
 
