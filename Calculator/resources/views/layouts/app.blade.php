9440a64b793fe51eb9aa058f9182c6084c494022
69safe <endokun32@yahoo.co.jp>
2021-08-30 21:51:12 +0900

    AC、C機能を実装

--- Calculator/resources/views/layouts/app.blade.php
+++ Calculator/resources/views/layouts/app.blade.php
@@ -18,81 +18,98 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
         <script type="text/javascript">
             function test(obj) {
+                let myTotal = 0;
+                let myInput = "";
+                let myCalc = "+";
+                let myFlg = 1;
+
                     switch(obj.innerText){
                         case "AC":
-
+                        myTotal = 0;	// 合計クリア
+                        myCalc = "+";	// 演算子クリア
+                        myInput = "";	// 現在入力している値をクリア
+                        document.getElementById('number').innerHTML = myTotal;
                         break;
 
                         case "C":
+                            if(myTotal = 0){ //既に入力した数値が無い場合
+                                myTotal = 0;	// 合計クリア
+                                myInput = 0;	// 現在入力している値をクリア
+                            }else{
+                                myInput = 0;	// 現在入力している値をクリア
+                            }
 
+                            document.getElementById('number').innerHTML = myInput;
                         break;
 
                         case "%":
-
+                            myInput = myInput / 100;	// 現在入力している値を1/100に
+                            var array = myInput.toString().split('.');
+                            document.getElementById('number').innerHTML = myInput(array[1].length);;
                         break;
 
                         case "÷":
-
+                            document.getElementById('number').innerHTML = obj.innerText;
                         break;
 
                         case "1":
-
+                            document.getElementById('number').innerHTML = obj.innerText;
                         break;
 
                         case "2":
-
+                            document.getElementById('number').innerHTML = obj.innerText;
                         break;
 
                         case "3":
-
+                            document.getElementById('number').innerHTML = obj.innerText;
                         break;
 
                         case "×":
-
+                            document.getElementById('number').innerHTML = obj.innerText;
                         break;
 
                         case "4":
-
+                            document.getElementById('number').innerHTML = obj.innerText;
                         break;
 
                         case "5":
-
+                            document.getElementById('number').innerHTML = obj.innerText;
                         break;
 
                         case "6":
-
+                            document.getElementById('number').innerHTML = obj.innerText;
                         break;
 
                         case "-":
-
+                            document.getElementById('number').innerHTML = obj.innerText;
                         break;
 
                         case "7":
-
+                            document.getElementById('number').innerHTML = obj.innerText;
                         break;
 
                         case "8":
-
+                            document.getElementById('number').innerHTML = obj.innerText;
                         break;
 
                         case "9":
-
+                            document.getElementById('number').innerHTML = obj.innerText;
                         break;
 
                         case "+":
-
+                            document.getElementById('number').innerHTML = obj.innerText;
                         break;
 
                         case "0":
-
+                            document.getElementById('number').innerHTML = obj.innerText;
                         break;
 
                         case ".":
-
+                            document.getElementById('number').innerHTML = obj.innerText;
                         break;
 
                         case "=":
-
+                            document.getElementById('number').innerHTML = obj.innerText;
                         break;
                     }
 
