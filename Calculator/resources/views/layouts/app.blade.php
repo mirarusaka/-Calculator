4928586000146ed479b2a78feb41188cf9f8ef66
69safe <endokun32@yahoo.co.jp>
2021-11-28 19:10:39 +0900

    googlefont適用

--- Calculator/resources/views/layouts/app.blade.php
+++ Calculator/resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <title>Oyama Calculator - @yield('title')</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css?family=Fredoka+One" rel="stylesheet">
   <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
